e0fcbddc05b063a41e78f7909060c877b963ebe6
Carlos Filipe <thecarlosfilipe@gmail.com>
2022-06-16 18:33:58 -0300

    🎉 Begin a project

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text, View, StatusBar, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Routes from './src/routes';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
+      <StatusBar backgroundColor="#232630" barStyle="light-content" />
       <StatusBar style="auto" />
-    </View>
+      <Routes />     
+    </NavigationContainer>
   );
 }
 
