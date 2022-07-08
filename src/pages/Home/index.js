2201f92c7fe957ec4abde6106a1b0e4932ce0c7f
Carlos Filipe <thecarlosfilipe@gmail.com>
2022-07-08 10:39:19 -0300

    🚧 Work in progress

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,27 +1,46 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, SafeAreaView, TouchableOpacity } from 'react-native';
 
 import { useNavigation } from '@react-navigation/native';
+import { Feather } from '@expo/vector-icons';
 
 export default function Home() {
 
   const navigation = useNavigation();
 
   return (
-    <View style={styles.container}>
-      <Text>Home</Text>
-      <Button
-        title='Ir para detalhes'
-        onPress={() => navigation.navigate('Details')}
-      />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.name}>DevBlog</Text>
+
+        <TouchableOpacity>
+          <Feather 
+            name="search"
+            size={24}
+            color="#fff"
+          />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#232630'
+  },
+  header: {
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'space-between',
+    marginHorizontal: 18,
+    marginTop: 18,
+    marginBottom: 24
+  },
+  name: {
+    fontSize: 28,
+    color: '#fff',
+    fontWeight: 'bold'
   }
 })
