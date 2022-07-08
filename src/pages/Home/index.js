49cbf645fee566589b2aff48cb142b34524a1772
Carlos Filipe <thecarlosfilipe@gmail.com>
2022-07-07 22:02:38 -0300

    ✨ Create new components

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+import { useNavigation } from '@react-navigation/native';
 
 export default function Home() {
+
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
       <Text>Home</Text>
+      <Button
+        title='Ir para detalhes'
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   )
 }
--- src/routes.js
+++ src/routes.js
@@ -2,18 +2,59 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Home from './pages/Home';
+import Details from './pages/Details';
+import CategoryPosts from './pages/CategoryPosts';
+import Search from './pages/Search';
 
 const Stack = createNativeStackNavigator();
 
 function Routes() {
   return (
     <Stack.Navigator>
-      <Stack.Screen
-        name="Home"
-        component={Home}
+      <Stack.Screen 
+        name="Home" 
+        component={Home} 
+        options={{
+          headerShown: false
+        }} 
+      />
+
+      <Stack.Screen 
+        name="Details" 
+        component={Details}
+        options={{
+          title: 'Detalhes',
+          headerTintColor: '#fff',
+          headerStyle: {
+            backgroundColor: '#232630'
+          }
+        }} 
+      />
+
+      <Stack.Screen 
+        name="Category" 
+        component={CategoryPosts} 
+        options={{
+          headerTintColor: '#fff',
+          headerStyle: {
+            backgroundColor: '#232630'
+          }
+        }}
+      />
+
+      <Stack.Screen 
+        name="Search" 
+        component={Search} 
+        options={{
+          title: 'Procurando algo?',
+          headerTintColor: '#fff',
+          headerStyle: {
+            backgroundColor: '#232630'
+          }
+        }}
       />
     </Stack.Navigator>
-  )
+  );
 }
 
 export default Routes;
